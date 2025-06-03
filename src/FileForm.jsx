e3210c9b2669f5203432b8359057d012399c3be3
Leonhardt, Jan (jaleo101) <jan.leonhardt@hhu.de>
2025-06-03 14:22:11 +0200

    feat(file): :sparkles: add file input area
    Adds a file input area, that can be clicked on or dropped on to add a new file to be read

---
+++ src/FileForm.jsx
@@ -0,0 +1,85 @@
+import { useRef, useState } from "react";
+
+export default function FileForm({ onRead }) {
+    const inputRef = useRef(null);
+    const [visualState, setVisualState] = useState('default');
+    const [currentFile, setCurrentFile] = useState(null);
+
+    const confirmFile = f => {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            onRead(e.target.result);
+        };
+        reader.readAsText(f);
+    }
+
+    const onNewFile = f => {
+        console.log(`new file: ${f.name}`);
+        setCurrentFile(f);
+    }
+
+    const onButtonClick = e => {
+        e.preventDefault();
+        e.stopPropagation();
+        const input = inputRef.current;
+        input.click()
+    }
+
+    const onDragEnter = e => {
+        e.preventDefault();
+        e.stopPropagation();
+    }
+
+    const onDragOver = e => {
+        e.preventDefault();
+        e.stopPropagation();
+        if (visualState === 'default') {
+            setVisualState('hovering');
+        }
+    }
+
+    const onDragLeave = e => {
+        e.preventDefault();
+        e.stopPropagation();
+        setVisualState("default");
+    }
+
+    const onDrop = e => {
+        e.stopPropagation();
+        e.preventDefault();
+        const f = e.dataTransfer.files[0];
+        setVisualState("confirm");
+        onNewFile(f);
+    }
+
+    const onFileChange = e => {
+        const f = e.target.files[0];
+        onNewFile(f);
+        setVisualState("confirm");
+    }
+
+    const onConfirm = () => {
+        confirmFile(currentFile);
+        setVisualState("default");
+    }
+
+    return (
+        <div onDragLeave={onDragLeave} onDragOver={onDragOver} onDragEnter={onDragEnter} onDrop={onDrop} style={{ borderStyle: "dotted", borderWidth: "5px 5px 5px 5px", borderColor: (visualState === 'default' ? "black" : "green"), width: "100%", height: "100%" }}>
+            {
+                (visualState === 'default') ?
+                    <><p>Ziehe eine Datei hierhin oder drücke den Button</p>
+                        <input onChange={onFileChange} ref={inputRef} type="file" hidden />
+                        <button onClick={onButtonClick} type="button">Neue Datei hochladen</button></> :
+                    (visualState === 'hovering') ?
+                        (<>
+                            <p>Loslassen zum hochladen</p>
+                        </>) :
+                        <>
+                            <p>Datei {currentFile.name} wurde hochgeladen. Soll sie benutzt werden?</p>
+                            <button onClick={onConfirm}>Bestätigen</button>
+                        </>
+            }
+
+        </div>
+    );
+}
