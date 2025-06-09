075896065f235b7686032c92132c880a993b6203
Leonhardt, Jan (jaleo101) <jan.leonhardt@hhu.de>
2025-06-09 18:20:11 +0200

    feat(algorithms): :sparkles: add algorithmic support for basic blocks

---
+++ src/basic-blocks.js
@@ -0,0 +1,59 @@
+export default function convertToBasicBlocks(tac) {
+    let [first, ...restTac] = tac;
+    let leaderIds = new Set([first.label]);
+    for (let instr of restTac) {
+        if (instr.type === 'jmp' || instr.type === 'cjmp') {
+            let target = instr.result.val;
+            leaderIds.add(target);
+            if (instr !== tac.at(-1)) leaderIds.add(instr.label + 1);
+        }
+    }
+    let leaders = [...leaderIds].sort();
+    // push sentinal value for easier slicing
+    leaders.push(tac.length);
+    let blocks = [];
+    for (let i = 0; i < leaders.length - 1; i++) {
+        let start = leaders[i];
+        let end = leaders[i + 1];
+        let blockInstr = tac.slice(start, end);
+        blocks.push(new BasicBlock(blockInstr, i));
+    }
+    for (let i = 0; i < blocks.length; i++) {
+        let block = blocks[i];
+        let lastInstr = block.instructions.at(-1);
+        if (lastInstr === 'cjmp' || lastInstr === 'jmp') {
+            let target = lastInstr.result.val;
+            // must exist because all instructions are in exactly one block
+            let targetBlock = blocks.filter(b => b.instructions.map(i => i.label === target))[0];
+            block.addTarget(targetBlock.blockId);
+        }
+        if (lastInstr !== 'jmp') {
+            block.addTarget(block.blockId + 1);
+        }
+    }
+    return blocks;
+}
+
+export class BasicBlock {
+
+    blockId;
+    instructions;
+    targets;
+
+    constructor(instructions, blockId) {
+        this.instructions = instructions;
+        this.blockId = blockId;
+        this.targets = [];
+    }
+
+    addTarget(otherBlockId) {
+        this.targets.push(otherBlockId);
+    }
+
+    toString() {
+        return this.instructions.map(i => i.toString()).join('\n');
+    }
+
+}
+
+
