f5e77921044c31b0147a8beacf40990916271820
Leonhardt, Jan (jaleo101) <jan.leonhardt@hhu.de>
2025-06-24 18:10:58 +0200

    perf(graph): :zap: remove useless print statements from ElkEdge

--- src/ElkEdge.jsx
+++ src/ElkEdge.jsx
@@ -6,21 +6,11 @@ const style = {
     stroke: '#FF0072',
 }
 
-const internal = {
-    type: MarkerType.ArrowClosed,
-    width: 20,
-    height: 20,
-    color: '#FF0072',
-}
-
 
 export function ElkEdge({ id, data, markerEnd }) {
     const path = data.points
         .map((p, i) => (i === 0 ? `M${p.x},${p.y}` : `L${p.x},${p.y}`))
         .join(' ');
 
-    console.log(markerEnd);
-
-    console.log(internal);
     return <BaseEdge id={id} style={style} path={path} markerEnd={markerEnd} />;
 }
